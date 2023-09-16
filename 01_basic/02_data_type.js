7c522dfd456bae5643eaf2f30bbc7683e9e7abf3
AKASH RAI <120483379+Akashraidev@users.noreply.github.com>
2023-09-16 09:33:15 +0000

    Data types

---
+++ 01_basic/02_data_type.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all js code as new version
+
+// alert(3+3)  //it will throw error because we use node js ,not broswer. If we write this code in console of browser it print 6
+
+console.log(3+3); //6
+
+let name = "Akash"
+let age = 29
+let isLogged = false
+
+/*
+number => 2^53
+bigint (bde number ke liye ex-trading,facebook)
+string => ""
+boolean => true/false
+null => standalone
+undefined => value not assign till now
+symbol => use for 'unique'
+
+object
+
+typeof => use check datatpe
+*/
+console.log(typeof "akash");
+
+console.log(typeof age);
