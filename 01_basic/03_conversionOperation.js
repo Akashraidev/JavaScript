23250b2530839e53521129c47902666d51dd691e
AKASH RAI <120483379+Akashraidev@users.noreply.github.com>
2023-09-16 15:08:31 +0000

    conversion of data type

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let scroe = "33abc"   //  let scroe = null  // o/p => 0   // let scroe = true   // o/p => 1 
+
+console.log(typeof scroe);
+console.log(typeof scroe);
+
+// coversion datatype --> string to number
+
+let valueNumber = Number(scroe)
+console.log(valueNumber);
+console.log(typeof valueNumber);
+console.log(typeof valueNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1 false = 0
+
+let isLogged = 1
+let booleanIsLogged = Boolean(isLogged)
+
+console.log(booleanIsLogged);
+
+/*
+ 1=> true   0=>false
+ "" => false
+"Akash" => true
+*/
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
