6234b0af3965838c92196c453ec63e35ad7fcc16
AKASH RAI <120483379+Akashraidev@users.noreply.github.com>
2023-09-23 06:38:36 +0000

    Break and Continue statement

--- 05_iteration/forloop.js
+++ 05_iteration/forloop.js
@@ -17,5 +17,37 @@ for (let i = 1; i <= 10; i++) {
     console.log(`Inner loop value ${j} and inner loop ${i}`);
     //console.log(i + '*' + j + ' = ' + i*j );
    }
+}
+
+
+let myArray = ["flash", "batman", "superman"]
+console.log("Array lenght is :-"+myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log("Array element:",element);
     
+}
+
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        break
+    }
+   console.log(`Value of i is ${index}`);
+}
+
+
+
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 10) {
+        console.log(`Detected 10`);
+        continue
+    }
+   console.log(` continue Statement :Value of i is ${index}`);
+
 }
